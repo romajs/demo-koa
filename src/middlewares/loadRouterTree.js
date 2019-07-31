951a29d0cf201db73aef5f755dc4fd192de50e1c
Romualdo Santos <romajslit@gmail.com>
2019-07-31 18:14:33 -0300

    Fixing load router tree filters, now supports js regex

--- src/middlewares/loadRouterTree.js
+++ src/middlewares/loadRouterTree.js
@@ -4,40 +4,44 @@ const path = require('path');
 const Router = require('koa-router');
 const slash = require('slash');
 
+const isValidFileName = (data, filters) => {
+  const valid = filters.some(filter => filter.test(data));
+  return valid;
+}
+
 const dirTreeSync = (dir, filters) => fs.readdirSync(dir)
-  .filter(data => (filters && filters.length > 0 && filters.indexOf(data) > -1) || true) // FIXME:
   .map(data => {
     const file = path.join(dir, data);
-    return fs.statSync(file, filters).isDirectory()
+    return fs.statSync(file).isDirectory()
       ? { file, tree: dirTreeSync(file) }
       : { file };
   });
 
-const loadRouteTree = (dirTree, getRoutePath, parentRouter) => {
+const loadRouteTree = (dirTree, getRoutePath, filters, parentRouter) => {
   dirTree.forEach(({ file, tree }) => {
     const routePath = getRoutePath(file);
     if (tree) {
-      loadRouteTree(tree, getRoutePath, parentRouter);
-    } else {
+      loadRouteTree(tree, getRoutePath, filters, parentRouter);
+    } else if (isValidFileName(file, filters)) {
       const subRouter = require(file);
       parentRouter.use(routePath, subRouter.routes());
-      console.log(`Loaded "${file}" to "${routePath}".`);
+      console.log(`Loaded "${routePath}" from "${file}".`);
     }
   });
   return parentRouter;
 };
 
-const loadRouterTree = (apiPath = '', apiPrefix = '', fileName = 'router.js') => {
+const loadRouterTree = (apiPath = '', apiPrefix = '', filters = [/.*\.js/i]) => {
   const callerDirname = path.dirname(parentModule());
   const apiBasename = path.normalize(apiPath);
   const apiDirPath = path.join(callerDirname, apiBasename);
   console.info(`Loading router tree from "${apiDirPath}".`);
 
-  const apiDirTree = dirTreeSync(apiDirPath, [fileName]);
+  const apiDirTree = dirTreeSync(apiDirPath);
   // console.debug('apiDirTree:', apiDirTree);
 
   const getRoutePath = file => apiPrefix + slash(path.dirname(file).replace(apiDirPath, ''));
-  const apiRouterTree = loadRouteTree(apiDirTree, getRoutePath, new Router());
+  const apiRouterTree = loadRouteTree(apiDirTree, getRoutePath, filters, new Router());
   // console.debug('apiRouterTree:', apiRouterTree);
 
   return apiRouterTree.routes();
