6fdaa44ae5a451ec27eb1e1486d48975adb3b44d
Romualdo Santos <romajslit@gmail.com>
2019-08-03 17:46:56 -0300

    Creating sign key with gpg in gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,33 @@
+const fs = require('fs');
 const pkg = require('pkg');
+const gpg = require('gpg');
+const path = require('path');
+
+const BUILD_DIR = path.join(__dirname, 'build');
+
+const noop = () => null;
 
 const build = async (done) => {
-  await pkg.exec(['.', '--out-path=build', '--scripts', 'src/api/*"']);
+  console.info('Cleaning build directory existing files.');
+  fs.readdirSync(BUILD_DIR).forEach(fileName => {
+    fs.unlinkSync(path.join(BUILD_DIR, fileName));
+  });
+
+  console.info('Creating output build packages.');
+  await pkg.exec(['.', `--out-path=${path.basename(BUILD_DIR)}`, '--scripts', 'src/api/*"']);
+
+  console.info('Creating sign keys.');
+  fs.readdirSync(BUILD_DIR).forEach(buildName => {
+    const inputPath = path.join(BUILD_DIR, buildName);
+    const outputPath = `${inputPath}.sig`;
+
+    console.info(`Creating sign key for build ${buildName}.`);
+    gpg.callStreaming(inputPath, outputPath, ['--detach-sign'], noop);
+  });
+
   done();
 };
 
-exports.default = build;
+module.exports = {
+  build,
+};
